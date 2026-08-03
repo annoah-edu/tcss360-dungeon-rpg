d80c47f6ba1d749826592b1833a449861051d0b1
Noah <aannoah@gmail.com>
2026-08-02 23:51:44 -0700

    Reworked Trenten's attack mechanics and added simple knockback

--- player.gd
+++ player.gd
@@ -2,20 +2,28 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 
+@export var atk_dmg: int = 1
+@export var knockback_strength: int = 130
 @export var atk_rate: float = 0.5
 
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var weapon: Node2D = $WeaponHandle
-@onready var weapon_sprite: Sprite2D = $WeaponHandle/Weapon
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
-@onready var swing: Sprite2D = $WeaponHandle/Swing
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D # The visual character sprite
+@onready var weapon: Node2D = $WeaponHandle # The weapon handle to rotate around the mouse
+@onready var weapon_sprite: Sprite2D = $WeaponHandle/Weapon # The weapon sprite itself
+@onready var anim_player: AnimationPlayer = $AnimationPlayer # The animation player handling weapon animations
+@onready var swing: Sprite2D = $WeaponHandle/Swing # The swing effect
+@onready var weapon_hitbox: Area2D = $WeaponHandle/Hitbox # The physics area that will poll for enemies
 
 var x_direction: float
 var y_direction: float
 var atk_cooldown: float = 0
+var enemies_in_range: Array[Enemy] # The array of enemies inside the physics area
 
 func _ready() -> void:
-	_hide_swing()
+	_hide_swing() # Hide the swinging sprite in case it wasn't hidden in-editor yet
+	
+	# Connect the weapon hitbox's signals to functions in this script
+	weapon_hitbox.body_entered.connect(_enemy_entered.bind())
+	weapon_hitbox.body_exited.connect(_enemy_exited.bind())
 
 func _physics_process(_delta: float) -> void:
 	_handle_movement()
@@ -26,6 +34,10 @@ func _physics_process(_delta: float) -> void:
 func _process(delta: float) -> void:
 	atk_cooldown -= delta
 	_handle_attacking()
+
+
+# Movement and rotation related functions
+
 
 ## Uses the CharacterController and input axis to apply movement. Also flips the Sprite when moving backwards.
 func _handle_movement() -> void:
@@ -69,6 +81,10 @@ func _handle_weapon_rotation() -> void:
 	else:
 		weapon.scale.x = 1
 
+
+# Attack related functions
+
+
 ## Plays attacking animations when the player is attacking.
 func _handle_attacking() -> void:
 	if Input.is_action_pressed("attack") and atk_cooldown <= 0:
@@ -88,3 +104,18 @@ func _show_swing() -> void:
 ## Hides the swing effect.
 func _hide_swing() -> void:
 	swing.visible = false
+
+## Adds an enemy to the array of enemies in range. I typecast the body to the Enemy class for the IDE so we can get things like autocompletion.
+func _enemy_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		enemies_in_range.append(body.get_parent() as Enemy)
+
+## Removes an enemy in the array of enemies in range.
+func _enemy_exited(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		enemies_in_range.erase(body.get_parent() as Enemy)
+
+## Damages enemies inside the current weapon's range by looping over the array of enemies.
+func _damage_enemies() -> void:
+	for enemy in enemies_in_range:
+		enemy.take_damage(atk_dmg, weapon_hitbox.global_position, knockback_strength)
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,17 +1,32 @@
 extends CharacterBody2D
+class_name Enemy
 
+@export var max_health: int = 3
+@export var knockback_recovery_spd: int = 500
 @export var speed: float = 80.0
+
 @export var wander_radius: float = 300.0
 @export var min_wait: float = 2.0
 @export var max_wait: float = 3.0
 
 @onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var healthbar: TextureProgressBar = $HealthBar
+
+var health: int
 
 var start_position: Vector2
 var is_waiting: bool = false
+var knockback_velocity: Vector2 = Vector2.ZERO # Used on top of navigation to apply knockback
 
 func _ready() -> void:
+	health = max_health
+	
+	# Hide the healthbar initially, and set its max value
+	healthbar.visible = false
+	healthbar.max_value = max_health
+	
+	# Start the navigation
 	start_position = global_position
 	nav_agent.path_desired_distance = 5.0
 	nav_agent.target_desired_distance = 5.0
@@ -19,17 +34,34 @@ func _ready() -> void:
 	await get_tree().physics_frame
 	_pick_new_target()
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	# Apply knockback first before navigation
+	if knockback_velocity.length() > 1.0:
+		velocity = knockback_velocity
+		knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, delta * knockback_recovery_spd)
+		move_and_slide()
+		return
+	
+	# If idling, don't update navigation
 	if is_waiting:
 		return
-
+	
+	# If the navigation path is finished, find a new target position
 	if nav_agent.is_navigation_finished():
 		_wait_then_pick_new_target()
 		return
-
+	
+	# Determine the next point in the navigation path, and move there
 	var next_point: Vector2 = nav_agent.get_next_path_position()
 	var direction: Vector2 = global_position.direction_to(next_point)
 	nav_agent.set_velocity(direction * speed)
+
+
+func _process(delta: float) -> void:
+	animation.modulate = animation.modulate.lerp(Color.WHITE, delta * 10)
+
+# Navigation and pathfinding functions
+
 
 func _on_velocity_computed(safe_velocity: Vector2) -> void:
 	velocity = safe_velocity
@@ -39,6 +71,7 @@ func _on_velocity_computed(safe_velocity: Vector2) -> void:
 		animation.flip_h = false
 	move_and_slide()
 
+## Stops moving, waits a random amount of time, then pathfinds somewhere else.
 func _wait_then_pick_new_target() -> void:
 	is_waiting = true
 	animation.play("idle")
@@ -47,6 +80,7 @@ func _wait_then_pick_new_target() -> void:
 	is_waiting = false
 	_pick_new_target()
 
+## Picks a new target within a specified range, and sets the navigation agent's goal to that
 func _pick_new_target() -> void:
 	var random_offset := Vector2(
 		randf_range(-wander_radius, wander_radius),
@@ -54,3 +88,20 @@ func _pick_new_target() -> void:
 	)
 	nav_agent.target_position = global_position + random_offset
 	animation.play("moving")
+
+
+# Attacking and damage functions
+
+## Takes a specified amount of damage, and dies if health is below 0
+func take_damage(amount: int, source: Vector2, knockback_strength: int) -> void:
+	health -= amount
+	if health <= 0:
+		queue_free()
+		return
+	
+	var direction: Vector2 = (global_position - source).normalized()
+	knockback_velocity = direction * knockback_strength
+	
+	healthbar.visible = true
+	healthbar.value = health
+	animation.modulate = Color.RED
